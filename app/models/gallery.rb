e90bbeef0a017f9b2b490fe4f038d9ecf5058300
dbpatnodee <dbpatnode@hotmail.com>
2020-06-10 08:34:16 -0700

    Done.

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -2,9 +2,32 @@ class Gallery
 
   attr_reader :name, :city
 
+  @@all = []
   def initialize(name, city)
     @name = name
     @city = city
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  # * `Gallery#paintings`
+  #   * Returns an `array` of all paintings in a gallery
+  def paintings
+    Painting.all.select {|painting| painting.gallery == self}
+  end
+
+  # * `Gallery#artists`
+    # * Returns an `array` of all artists that have a painting in a gallery
+
+  def artists
+    paintings.map {|painting| painting.artist}
+  end
+
+  def artist_names
+    artists.map {|artist| artist.name}
   end
 
 end
--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,12 +1,29 @@
 class Painting
 
-  attr_reader :title, :price
+  attr_reader :title, :price, :artist
+  attr_accessor :gallery
 
   @@all = []
 
-  def initialize(title, price)
+  def initialize(title, price, artist, gallery)
     @title = title
     @price = price
+    @artist = artist
+    @gallery = gallery
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  # Painting.total_price`
+    # * Returns an `integer` that is the total price of all paintings
+  def self.total_price
+    total = all.map {|painting|painting.price} 
+    total.sum
+  end
+
+  
 
 end
